a28ba9be0f927c9527ba5258fcebcfecf74f3e18
Cucumis-Glaciei <75741031+Cucumis-Glaciei@users.noreply.github.com>
2022-08-09 22:35:58 +0900

    Usability test of CString

--- FileSectCopy/FileSectCopy.cpp
+++ FileSectCopy/FileSectCopy.cpp
@@ -8,10 +8,11 @@
 #include <filesystem>
 #include <locale>
 #include <tchar.h>
+#include <atlstr.h>
 
 #include "FileClusterFragmentsRetriever.h"
 
-int wmain(int argc, TCHAR** argv)
+int _tmain(int argc, TCHAR** argv)
 {
 	if (argc < 3) {
 		_tprintf(_T("Usage: FileSectCopy infile outfile\n"));
@@ -32,13 +33,13 @@ int wmain(int argc, TCHAR** argv)
 		return -1;
 	}
 
-	puts("-------------------------------------------");
-	TCHAR volume_path[32];
-	_stprintf_s(volume_path, 32, _T("\\\\.\\%c:"), clusterdistrib->getDriveLetter());
+	_putts(_T("-------------------------------------------"));
+	CString volume_path_str;
+	volume_path_str.Format(_T("\\\\.\\%c:"), clusterdistrib->getDriveLetter());
 
 	// Get File Handle for the volume
 	HANDLE volume_handle = CreateFile(
-		volume_path,
+		(LPCTSTR) volume_path_str,
 		GENERIC_READ,
 		FILE_SHARE_READ | FILE_SHARE_WRITE,
 		NULL,
@@ -48,6 +49,8 @@ int wmain(int argc, TCHAR** argv)
 	);
 
 	if (volume_handle == INVALID_HANDLE_VALUE) {
+		DWORD err = GetLastError();
+		_tprintf_s(_T("Failed to obtain volume handle: %s; Err code %d\n"), (LPCTSTR)volume_path_str, err);
 		return -1;
 	}
 
@@ -106,95 +109,9 @@ int wmain(int argc, TCHAR** argv)
 	CloseHandle(volume_handle);
 	out_file.close();
 
-
-
-	//delete clusterdistrib;
+	delete clusterdistrib;
 	return 0;
 }
-
-int wmain_old(int argc, TCHAR** argv)
-{
-	if (argc < 2) {
-		wprintf(L"Usage: FileSectCopy file\n");
-		return -1;
-	}
-
-	std::cout << "Hello World!\n";
-
-	TCHAR* file_path_str = argv[1];
-	std::filesystem::path file_path = file_path_str;
-	char drive_letter = file_path.root_name().string().at(0);
-	if (!std::isalpha(drive_letter)) {
-		std::cout << "Target File: " << file_path.string() << " does not have Drive Letter" << std::endl;
-		return -1;
-	}
-	std::cout << "Target File: " << file_path.string() << "\nDrive Letter: " << drive_letter << std::endl;
-
-
-	HANDLE file_handle = CreateFile(
-		file_path_str,
-		0x00,
-		FILE_SHARE_READ,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL
-	);
-
-	if (file_handle == INVALID_HANDLE_VALUE) {
-		std::cout << "Failed to obtain file handle of " << file_path_str << std::endl;
-		return -1;
-	}
-	else {
-		std::cout << "Obtained file handle of " << file_path_str << ": " << file_handle << std::endl;
-	}
-
-	STARTING_VCN_INPUT_BUFFER vcn_input{};
-	vcn_input.StartingVcn.QuadPart = 0;
-	std::vector<unsigned char> outvec(128);
-	DWORD bytes_returned = 0;
-	DWORD errorcode = 0;
-	BOOL status_deviceiocontrol = FALSE;
-
-	do {
-		status_deviceiocontrol = DeviceIoControl(
-			file_handle,
-			FSCTL_GET_RETRIEVAL_POINTERS,
-			&vcn_input,
-			sizeof vcn_input,
-			outvec.data(),
-			outvec.size(),
-			&bytes_returned,
-			NULL
-		);
-		errorcode = GetLastError();
-		if (errorcode == ERROR_MORE_DATA || errorcode == ERROR_INSUFFICIENT_BUFFER) {
-			outvec.resize(outvec.size() * 2);
-		}
-		else if (errorcode != NO_ERROR) {
-			return -1;
-		}
-		std::cout << "Returned bytes of FSCTL_GET_RETRIEVAL_POINTERS: " << bytes_returned << std::endl;
-		std::cout << "Status of FSCTL_GET_RETRIEVAL_POINTERS: " << status_deviceiocontrol << std::endl;
-		std::cout << "Error Code of FSCTL_GET_RETRIEVAL_POINTERS: " << errorcode << std::endl;
-	} while (status_deviceiocontrol != TRUE);
-
-	for (unsigned int i = 0; i < bytes_returned; i++) {
-		printf_s("%02X ", outvec[i]);
-	}
-	putchar('\n');
-
-	// Verification of casting into RETRIEVAL_POINTERS_BUFFER
-	RETRIEVAL_POINTERS_BUFFER* retbuf = (RETRIEVAL_POINTERS_BUFFER*)outvec.data();
-
-	printf_s("Extent Count=%d, Starting VCN=%lld\n", retbuf->ExtentCount, retbuf->StartingVcn.QuadPart);
-	for (unsigned int i = 0; i < retbuf->ExtentCount; i++) {
-		printf_s("Next VCN=0x%llX LCN=%llX\n", retbuf->Extents[i].NextVcn.QuadPart, retbuf->Extents[i].Lcn.QuadPart);
-	}
-
-	CloseHandle(file_handle);
-}
-
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
 
